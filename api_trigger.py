50b717f1e4858d2e0b66dfe934e21033f65a5914
Danil3465 <daniljk09@gmail.com>
2026-05-11 19:05:23 +0700

    Create api_trigger.py

---
+++ api_trigger.py
@@ -0,0 +1,9 @@
+from email_parser import process_emails
+
+def main():
+    print("Запуск парсера по запросу...")
+    process_emails()
+    print("Готово!")
+
+if __name__ == "__main__":
+    main()
